a6483d866096b70ec410c6b82e1c872a54bac9ff
Richard Carter Colina <ricarcol.rc@gmail.com>
2026-02-16 04:29:40 +0000

    Initial Commit

---
+++ .emacs.d/init.el
@@ -0,0 +1,51 @@
+;; What is recentf in emacs?
+;; What is company in emacs?
+
+;; Set font
+(set-frame-font "Iosevka Nerd Font Mono")
+
+;; Set theme
+(load "~/.emacs.d/fleury-theme.el")
+(load-theme 'fleury t)
+
+(require 'package)
+(add-to-list 'package-archives
+			 '("melpa" . "https://melpa.org/packages/") t)
+;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+;; and `package-pinned-packages`. Most users will not need or want to do this.
+;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+(package-refresh-contents) ; <- a tad slow
+
+;; Download Evil
+(unless (package-installed-p 'evil)
+  (package-install 'evil))
+
+;; Enable Evil
+(require 'evil)
+(evil-mode 1)
+
+(setq inhibit-startup-message t)
+
+(tool-bar-mode 0)
+(menu-bar-mode 0)
+(scroll-bar-mode 0)
+(column-number-mode 1)
+(global-display-line-numbers-mode 1)
+
+;; ido
+(ido-mode 1)
+(ido-everywhere 1)
+;; (ido-ubiquitous-mode 1)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
