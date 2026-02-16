f047fc1248ee2081c489de25b74e264dec0f02b0
Richard Carter Colina <ricarcol.rc@gmail.com>
2026-02-16 19:41:01 +0000

    Add xenops

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -7,12 +7,17 @@
 
 (require 'package)
 (add-to-list 'package-archives
-			 '("melpa" . "https://melpa.org/packages/") t)
+  '("melpa" . "https://melpa.org/packages/") t)
 ;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
 ;; and `package-pinned-packages`. Most users will not need or want to do this.
 ;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
 (package-refresh-contents)
+
+(unless (package-installed-p 'xenops)
+  (package-install 'xenops))
+(add-hook 'latex-mode-hook #'xenops-mode)
+(add-hook 'LaTeX-mode-hook #'xenops-mode)
 
 ;; Download Evil
 (unless (package-installed-p 'evil)
@@ -33,13 +38,15 @@
 ;; ido
 (ido-mode 1)
 (ido-everywhere 1)
+(setq ido-auto-merge-work-directories-length -1)
 ;; (ido-ubiquitous-mode 1)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages nil))
+ '(package-selected-packages '(evil xenops)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
