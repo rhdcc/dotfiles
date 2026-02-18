9ef733fb2343e7c0224a7035ea328804f8f88c7b
Richard Carter Colina <ricarcol.rc@gmail.com>
2026-02-18 00:02:43 +0000

    Fix some errors

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -23,8 +23,6 @@
 ;; Download Evil
 (unless (package-installed-p 'evil)
   (package-install 'evil))
-
-;; Enable Evil
 (require 'evil)
 (evil-mode 1)
 
@@ -32,21 +30,10 @@
 (unless (package-installed-p 'org-roam)
   (package-install 'org-roam))
 (require 'org-roam)
-(make-directory "~/OrgRoam")
+(if (file-exists-p "~/OrgRoam") nil
+    (make-directory "~/OrgRoam"))
 (setq org-roam-directory "~/OrgRoam")
 (org-roam-db-autosync-mode)
-
-;; Recentf
-(recentf-mode 1)
-(setq recentf-max-menu-items 25)
-(setq recentf-max-saved-terms 25)
-(defun recentf-ido-find-file ()
-  "Find a recent file using Ido."
-  (interactive)
-  (let ((file (ido-completing-read "Choose recent file: " recentf-list nil t)))
-    (when file
-      (find-file file))))
-(global-set-key "\C-x\ \C-r" 'recentf-ido-find-file)
 
 (setq inhibit-startup-message t)
 (tool-bar-mode 0)
@@ -61,11 +48,25 @@
 (setq ido-auto-merge-work-directories-length -1)
 ;; (ido-ubiquitous-mode 1)
 
+;; Recentf
+(recentf-mode 1)
+(setq recentf-max-menu-items 25)
+(setq recentf-max-saved-terms 25)
+(defun recentf-ido-find-file ()
+  "Find a recent file using Ido."
+  (interactive)
+  (let ((file (ido-completing-read "Choose recent file: " recentf-list nil t)))
+    (when file
+      (find-file file))))
+(global-set-key "\C-x\ \C-r" 'recentf-ido-find-file)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+   '("~/OrgRoam/things_to_do.org" "/home/tau/OrgRoam/hello_roam.org"))
  '(package-selected-packages '(evil org-roam xenops)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
