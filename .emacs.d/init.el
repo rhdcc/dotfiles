064331381c93e400c9940c5e58ff3750d296b7d4
Richard Carter Colina <ricarcol.rc@gmail.com>
2026-02-27 07:29:10 +0000

    use package use-package

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,76 +1,70 @@
-;; Set font
-(set-frame-font "Iosevka Nerd Font Mono")
-
-;; Set theme
-(load "~/.emacs.d/fleury-theme.el")
-(load-theme 'fleury t)
-
 (require 'package)
 (add-to-list 'package-archives
   '("melpa" . "https://melpa.org/packages/") t)
-;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
-;; and `package-pinned-packages`. Most users will not need or want to do this.
-;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
 (package-refresh-contents)
 
-;; Xenops
-(unless (package-installed-p 'xenops)
-  (package-install 'xenops))
-(add-hook 'latex-mode-hook #'xenops-mode)
-(add-hook 'LaTeX-mode-hook #'xenops-mode)
+(use-package emacs
+  :init
+  (setq custom-file
+    (expand-file-name "custom.el" user-emacs-directory))
+  (load custom-file)
+  :config
+  ;; Font
+  (set-frame-font "Iosevka Nerd Font Mono")
 
-;; Download Evil
-(unless (package-installed-p 'evil)
-  (package-install 'evil))
-(require 'evil)
-(evil-mode 1)
+  ;; Theme
+  (load "~/.emacs.d/fleury-theme.el")
+  (load-theme 'fleury t)
+
+  ;; General Settings
+  (setq inhibit-startup-message t)
+  (tool-bar-mode 0)
+  (menu-bar-mode 0)
+  (scroll-bar-mode 0)
+  (column-number-mode 1)
+  (global-display-line-numbers-mode 1)
+
+  ;; Which key
+  (which-key-mode 1)
+
+  ;; Ido
+  ;; (ido-mode 1)
+  ;; (ido-everywhere 1)
+  ;; (setq ido-auto-merge-work-directories-length -1)
+  ;; (ido-ubiquitous-mode 1)
+
+  ;; Fido
+  (fido-mode 1)
+  (fido-vertical-mode 1)
+
+  ;; Recentf
+  (recentf-mode 1)
+  (setq recentf-max-menu-items 25)
+  (setq recentf-max-saved-terms 25))
+
+;; Xenops
+(use-package xenops
+  :init
+  (add-hook 'latex-mode-hook #'xenops-mode)
+  (add-hook 'LaTeX-mode-hook #'xenops-mode)
+  :config
+  (xenops-mode 1))
+
+;; LaTeX
+(use-package tex
+  :ensure auctex)
+
+;; Evil
+(use-package evil
+  :config
+  (evil-mode 1))
 
 ;; Org Roam
-(unless (package-installed-p 'org-roam)
-  (package-install 'org-roam))
-(require 'org-roam)
-(if (file-exists-p "~/OrgRoam") nil
-    (make-directory "~/OrgRoam"))
-(setq org-roam-directory "~/OrgRoam")
-(org-roam-db-autosync-mode)
-
-(setq inhibit-startup-message t)
-(tool-bar-mode 0)
-(menu-bar-mode 0)
-(scroll-bar-mode 0)
-(column-number-mode 1)
-(global-display-line-numbers-mode 1)
-
-;; Which key
-(which-key-mode 1)
-
-;; ido
-;; (ido-mode 1)
-;; (ido-everywhere 1)
-;; (setq ido-auto-merge-work-directories-length -1)
-;; (ido-ubiquitous-mode 1)
-
-;; Fido
-(fido-mode 1)
-(fido-vertical-mode 1)
-
-;; Recentf
-(recentf-mode 1)
-(setq recentf-max-menu-items 25)
-(setq recentf-max-saved-terms 25)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-agenda-files
-   '("~/OrgRoam/things_to_do.org" "/home/tau/OrgRoam/hello_roam.org"))
- '(package-selected-packages '(evil org-roam vertico xenops)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(use-package org-roam
+  :init
+  (if (file-exists-p "~/OrgRoam") nil
+      (make-directory "~/OrgRoam"))
+  :config
+  (setq org-roam-directory "~/OrgRoam")
+  (org-roam-db-autosync-mode))
